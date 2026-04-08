6dd833542dcdcc6ef6c7f799d34b4e032f61037c
YoungG71 <djbambino@hotmail.com>
2026-04-08 02:33:33 +0200

    chore: update favicon.png in dist directory

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/horizon-prevention/', // Remplacez par le nom de votre dépôt GitHub
+  base: '/', // Pour les domaines personnalisés sur GitHub Pages, la base est la racine
   plugins: [react()],
 })

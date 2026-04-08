58378c49c4689477a2e248da5a7167194ce87580
YoungG71 <djbambino@hotmail.com>
2026-04-08 02:09:47 +0200

    feat: Configure project for GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/horizon-prevention/', // Remplacez par le nom de votre dépôt GitHub
+  plugins: [react()],
+})
